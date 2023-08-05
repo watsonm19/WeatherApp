a312530d66e3632b4cd13a042c6182c8d5e310ea
watsonm19 <watsonm19@me.com>
2023-08-04 22:49:13 -0700

    removed environments files

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
-    XRapidApiHostHeaderName: 'X-RapidAPI-Host',
-    XRapidApiHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
-    XRapidApiKeyName: 'X-RapidAPI-Key',
-    XRapidApiKeyValue: 'cf1c64c7b7msh482cd900caa886ap185fb0jsnbd1cac808333'
-};
